ebb360c09d95fa1e105f4659e4719473a51ffa87
Donfack <Donfackfresnel4@gmail.com>
2022-03-16 11:29:36 +0100

    joinSeperate

---
+++ reactclient/src/App.js
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from 'react'
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const App = () => {
+  return ( 
+    <div className='App'>
+      
+    </div>
+   );
+}
+ 
+export default App;
